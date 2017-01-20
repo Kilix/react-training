429765451bedb75a4a0ef6c54daf8444800ec9fc
Augustin Le Fèvre <aulefevre@kilix.fr>
2017-01-20 16:24:04 +0100

    Base intro

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import './index.css';
+import './App.css';
+
+const App = () => {
+  const actors = ['George', 'Peter', 'Steven', 'Dave'];
+  return (
+    <div className="App">
+      <p className="App-intro">
+        Des gens sympas
+      </p>
+      <ul>
+        {actors.map(
+          (actor, index) => <li key={index}>{actor}</li>
+        )}
+      </ul>
+    </div>
+  );
+};
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+// pass actors as props to <App />
