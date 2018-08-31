812f9369cacd613d91947e2a8564db83ec0b947c
Amélie Lefèvre <alefevre@kilix.fr>
2018-08-31 14:40:32 +0200

    Remove Redux from main practice (#34)

--- src/corrections/mainPracticeCorrection/artistRow.js
+++ src/corrections/mainPracticeCorrection/artistRow.js
@@ -1,56 +1,37 @@
 import PropTypes from 'prop-types';
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
+import React from 'react';
 
 import {formatNumberToString} from './helpers';
-import {getArtistByName} from './selectors';
-import * as actions from './actions';
 
-const mapStateToArtistRowProps = (state, ownProps) => ({
-    artist: getArtistByName(state, ownProps.artistName),
-});
-const mapDispatchToArtistRowProps = {
-    openArtistCard: actions.openArtistCard,
-};
-
-class ArtistRow extends Component {
-    static propTypes = {
-        artist: PropTypes.object.isRequired,
-        artistName: PropTypes.string.isRequired,
-        openArtistCard: PropTypes.func.isRequired,
-    };
-
-    openArtistCard = () => {
-        const {artist, openArtistCard} = this.props;
+const ArtistRow = ({artist, openArtistCard}) => {
+    const openCard = () => {
         openArtistCard(artist.name);
         window.scrollTo(0, 0);
     };
 
-    render() {
-        const {artist} = this.props;
-        const imgUrl = artist.image[0]['#text'];
-        return (
-            <tr className="artist-row">
-                <td className="artist-row__image">
-                    <img src={imgUrl} alt={artist.name} />
-                </td>
-                <td className="artist-row__name" onClick={this.openArtistCard}>
-                    {artist.name}
-                </td>
-                <td>{formatNumberToString(artist.playcount)}</td>
-                <td>
-                    <a href={artist.url} target="_blank">
-                        {artist.url}
-                    </a>
-                </td>
-            </tr>
-        );
-    }
-}
+    const imgUrl = artist.image[0]['#text'];
 
-const enhance = connect(
-    mapStateToArtistRowProps,
-    mapDispatchToArtistRowProps
-);
-export default enhance(ArtistRow);
+    return (
+        <tr className="artist-row">
+            <td className="artist-row__image">
+                <img src={imgUrl} alt={artist.name} />
+            </td>
+            <td className="artist-row__name" onClick={openCard}>
+                {artist.name}
+            </td>
+            <td>{formatNumberToString(artist.playcount)}</td>
+            <td>
+                <a href={artist.url} target="_blank">
+                    {artist.url}
+                </a>
+            </td>
+        </tr>
+    );
+};
+
+ArtistRow.propTypes = {
+    artist: PropTypes.object.isRequired,
+    openArtistCard: PropTypes.func.isRequired,
+};
+
+export default ArtistRow;
