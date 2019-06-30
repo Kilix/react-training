0858b129168938b31ebbba593733e6445f858f08
Augustin Le Fèvre <aulefevre@kilix.fr>
2019-06-30 20:12:40 +0200

    Remove redux

--- redux/src/fullReduxApp/counter.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {connect} from 'react-redux';
-
-const mapStateToProps = state => ({
-    counter: state,
-});
-const Counter = ({counter}) => <div>{counter}</div>;
-export default connect(mapStateToProps)(Counter);
--- redux/src/reduxCourse/reducer.js
+++
@@ -1,3 +0,0 @@
-const reducer = () => {};
-
-export default reducer;
